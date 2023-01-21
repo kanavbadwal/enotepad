553bf2f45c7ac2d61f5af38cdfed0f9dae6f8246
Harkanav Bir Singh Badwal <30654005+kanavbadwal@users.noreply.github.com>
2023-01-21 14:40:04 +0530

    refactoring of code

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/enotepad";
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env.local") });
 
+const mongoURI = process.env.REACT_APP_DB;
+console.log(`../${process.env.REACT_APP_DB}`);
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
     console.log("Connected to mongoose successfully.");
